4a61d47e4f4b68132f4efadeb355d0dd46ae38d1
Dmitrii Podlesnykh <podlesnykh.dmitrii@gmail.com>
2020-06-09 17:52:41 +0300

    Update readme

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 
 Точка входа 
 <code>dog-shelter-example-main/src/main/java/com/example/shelter/Main.java</code>
+
+
+Dmitrii
+Podlesnykh
--- dog-shelter-example-main/src/main/java/com/example/shelter/Main.java
+++ dog-shelter-example-main/src/main/java/com/example/shelter/Main.java
@@ -5,5 +5,8 @@ public class Main
     public static void main(String... args)
     {
         System.out.println("Hello world!");
+
+
+
     }
 }
