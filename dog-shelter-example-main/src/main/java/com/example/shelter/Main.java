ff39821617a38483748880afddd54dfaaaabcaaa
vkonovki <Valeriia.Konovkina@t-systems.com>
2020-06-16 12:45:09 +0300

    add HW

--- dog-shelter-example-main/src/main/java/com/example/shelter/Main.java
+++ dog-shelter-example-main/src/main/java/com/example/shelter/Main.java
@@ -1,44 +1,57 @@
 package com.example.shelter;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 import com.example.shelter.animal.Dog;
+
 import java.util.List;
 
-public class Main
-{
-    public static void main(String... args)
-    {
+public class Main {
+    public static void main(String... args) {
         System.out.println("Выберете сохранять к коллекцию(1) или в массив(2)?");
         Scanner in = new Scanner(System.in);
         int selectedCase = in.nextInt();
-        if(selectedCase == 1)
-        {
+        if (selectedCase == 1) {
             caseWithArrayList();
-        } else if (selectedCase == 2)
-        {
+        } else if (selectedCase == 2) {
             caseWithArray();
-        } else
-        {
+        } else {
             System.out.println("некорректный ввод. Пока");
         }
     }
 
-    private static void caseWithArrayList()
-    {
+    private static void caseWithArrayList() {
         Scanner in = new Scanner(System.in);
 
         List<Dog> dogs = new ArrayList<>();
 
         String string = null;
 
-        while (!"exit".equals(string))
-        {
+
+
+        while (!"exit".equals(string)) {
             System.out.println("dog new name:");
             string = in.nextLine();
             Dog newDog = new Dog();
             newDog.name = string;
+            System.out.println("dog status:");
+           String  status = in.nextLine();
+            if (!status.equals("")) {
+
+                newDog.DogStatus = status;
+            }
+
+
+            System.out.println("dog time:");
+            String time1 = in.nextLine();
+            if (!time1.equals("")) {
+                newDog.DogTime = LocalDateTime.parse(time1);
+                //newDog.DogTime.getDayOfWeek();
+
+            }
+
 
             dogs.add(newDog);
         }
@@ -46,15 +59,13 @@ public class Main
         System.out.println(dogs);
     }
 
-    private static void caseWithArray()
-    {
+    private static void caseWithArray() {
         Scanner in = new Scanner(System.in);
 
         Dog[] dogs = new Dog[5];
         int index = 0;
         String string = null;
-        while (!"exit".equals(string))
-        {
+        while (!"exit".equals(string)) {
             System.out.println("dog new name:");
             string = in.nextLine();
             Dog newDog = new Dog();
@@ -64,8 +75,7 @@ public class Main
         }
 
         //вывод результата на экран
-        for (final Dog dog : dogs)
-        {
+        for (final Dog dog : dogs) {
             System.out.println(dog);
         }
     }
--- dog-shelter-example-main/src/main/java/com/example/shelter/animal/Dog.java
+++ dog-shelter-example-main/src/main/java/com/example/shelter/animal/Dog.java
@@ -1,17 +1,24 @@
 package com.example.shelter.animal;
 
+import java.time.LocalDateTime;
+
 /**
  * Important class
  */
 public class Dog
 {
+
     public String name = "Sharic";
+    public LocalDateTime DogTime = LocalDateTime.now();
+    public String DogStatus = "Принят";
+
 
     @Override
-    public String toString()
-    {
+    public String toString() {
         return "Dog{" +
                 "name='" + name + '\'' +
+                ", time='" + DogTime + '\'' +
+                ", status='" + DogStatus + '\'' +
                 '}';
     }
 }
