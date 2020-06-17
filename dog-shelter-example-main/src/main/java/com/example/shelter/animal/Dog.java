dece650431b6480c6a7a645b3abd2010a788afee
OlaMadeira <alinarossol"gmailcom>
2020-06-17 22:40:13 +0300

    dog date and status added

--- dog-shelter-example-main/src/main/java/com/example/shelter/animal/Dog.java
+++ dog-shelter-example-main/src/main/java/com/example/shelter/animal/Dog.java
@@ -1,17 +1,23 @@
 package com.example.shelter.animal;
 
+import java.time.LocalDate;
+
 /**
  * Important class
  */
 public class Dog
 {
     public String name = "Sharic";
+    public LocalDate arrivalDate = LocalDate.now();
+    public String dogStatus;
 
     @Override
-    public String toString()
-    {
+    public String toString() {
         return "Dog{" +
                 "name='" + name + '\'' +
+                ", arrivalDate=" + arrivalDate +
+                ", dogStatus='" + dogStatus + '\'' +
                 '}';
     }
 }
+
