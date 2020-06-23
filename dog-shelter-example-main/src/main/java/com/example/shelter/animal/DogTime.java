713325fbfc9747bbcbd9b70d836968cab74f58d0
mar-y-m <mar.minaeva@gmail.com>
2020-06-23 10:51:07 +0200

    unnecessary import removed

--- dog-shelter-example-main/src/main/java/com/example/shelter/animal/DogTime.java
+++ dog-shelter-example-main/src/main/java/com/example/shelter/animal/DogTime.java
@@ -3,7 +3,6 @@ package com.example.shelter.animal;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 public class DogTime {
