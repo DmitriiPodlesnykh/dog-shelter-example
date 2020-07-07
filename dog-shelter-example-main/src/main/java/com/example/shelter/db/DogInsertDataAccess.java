53574a2c269901b677b1fb62eb985782de592bb8
vkonovki <Valeriia.Konovkina@t-systems.com>
2020-07-07 14:47:22 +0300

    add HW

--- dog-shelter-example-main/src/main/java/com/example/shelter/db/DogInsertDataAccess.java
+++ dog-shelter-example-main/src/main/java/com/example/shelter/db/DogInsertDataAccess.java
@@ -2,34 +2,40 @@ package com.example.shelter.db;
 
 import java.sql.*;
 import java.time.LocalDateTime;
-import java.util.List;
-
+import java.util.*;
 import com.example.shelter.animal.Dog;
-import com.example.shelter.ArrayAndCollectionsExample;
 import com.example.shelter.animal.DogStatus;
-import java.time.LocalDateTime;
 
 
 public class DogInsertDataAccess {
-
-    public static void addNewDog(String name) {
+    public static void addNewDogByName(String name) {
+        Connection connection = null;
+        Statement statement = null;
         try (
-                Connection connection = DriverManager.getConnection(ShelterDataAccess.DB_CONNECTION);
+                Connection connection1 = DriverManager.getConnection(ShelterDataAccess.DB_CONNECTION);
                 PreparedStatement preparedStatement = connection.prepareStatement("insert into dogs (name) values (?);")) {
             preparedStatement.setString(1, name);
             preparedStatement.executeUpdate();
         } catch (SQLException e) {
             e.printStackTrace();
+        } finally {
+            try {
+                statement.close();
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
         }
 
     }
 
     /**
      * Добавить всех собак из коллекции в БД
-     *
+     * <p>
      * --@param dogs собаки
      */
-    public static void addNewDogs(String name, DogStatus status, LocalDateTime visit_time) {
+    public static void addNewDog(String name, DogStatus status, LocalDateTime visit_time) {
+
         try (
                 Connection connection = DriverManager.getConnection(ShelterDataAccess.DB_CONNECTION);
                 PreparedStatement preparedStatement = connection.prepareStatement("insert into dogs (name,status,visit_time) values (?,?,?);")) {
@@ -41,6 +47,19 @@ public class DogInsertDataAccess {
         } catch (SQLException e) {
             e.printStackTrace();
         }
+    }
 
+
+    /**
+     * Добавить всех собак из коллекции в БД
+     *
+     * @param dogs собаки
+     */
+    public static void addNewDogs(List<Dog> dogs) {
+
+        for (Dog dog : dogs) {
+
+            addNewDog(dog.name, dog.dogStatus, dog.visitTime);
+        }
     }
 }
