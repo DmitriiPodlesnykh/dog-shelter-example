0b2f1cfde4980299c0a17b75c9217843d6a5127a
Dmitrii Podlesnykh <podlesnykh.dmitrii@gmail.com>
2020-07-08 21:16:48 +0300

    Add lesson 2020 07 07

--- dog-shelter-example-main/src/main/java/com/example/shelter/db/DogInsertDataAccess.java
+++ dog-shelter-example-main/src/main/java/com/example/shelter/db/DogInsertDataAccess.java
@@ -3,63 +3,79 @@ package com.example.shelter.db;
 import java.sql.*;
 import java.time.LocalDateTime;
 import java.util.*;
+
 import com.example.shelter.animal.Dog;
 import com.example.shelter.animal.DogStatus;
 
+public class DogInsertDataAccess implements DogInsertDataAccessInterface
+{
+    private static final String INSERT_DOG_QUERY = "insert into dogs (name,status,visit_time) values (?,?,?);";
 
-public class DogInsertDataAccess {
-    public static void addNewDogByName(String name) {
+    public void addNewDogByName(String name)
+    {
         Connection connection = null;
         Statement statement = null;
         try (
                 Connection connection1 = DriverManager.getConnection(ShelterDataAccess.DB_CONNECTION);
-                PreparedStatement preparedStatement = connection.prepareStatement("insert into dogs (name) values (?);")) {
+                PreparedStatement preparedStatement = connection.prepareStatement("insert into dogs (name) values (?);"))
+        {
             preparedStatement.setString(1, name);
             preparedStatement.executeUpdate();
-        } catch (SQLException e) {
+        }
+        catch (SQLException e)
+        {
             e.printStackTrace();
-        } finally {
-            try {
+        }
+        finally
+        {
+            try
+            {
                 statement.close();
                 connection.close();
-            } catch (SQLException e) {
+            }
+            catch (SQLException e)
+            {
                 e.printStackTrace();
             }
         }
 
     }
 
+
     /**
      * Добавить всех собак из коллекции в БД
      * <p>
      * --@param dogs собаки
      */
-    public static void addNewDog(String name, DogStatus status, LocalDateTime visit_time) {
-
+    public void addNewDog(Dog dog)
+    {
         try (
                 Connection connection = DriverManager.getConnection(ShelterDataAccess.DB_CONNECTION);
-                PreparedStatement preparedStatement = connection.prepareStatement("insert into dogs (name,status,visit_time) values (?,?,?);")) {
-            preparedStatement.setString(1, name);
-            preparedStatement.setString(2, status.name());
-            preparedStatement.setTimestamp(3, Timestamp.valueOf(visit_time));
+                PreparedStatement preparedStatement = connection.prepareStatement(INSERT_DOG_QUERY)
+        )
+        {
 
+            preparedStatement.setString(1, dog.name);
+            preparedStatement.setString(2, dog.dogStatus.name());
+            preparedStatement.setTimestamp(3, Timestamp.valueOf(dog.visitTime));
             preparedStatement.executeUpdate();
-        } catch (SQLException e) {
+        }
+        catch (SQLException e)
+        {
             e.printStackTrace();
         }
     }
-
 
     /**
      * Добавить всех собак из коллекции в БД
      *
      * @param dogs собаки
      */
-    public static void addNewDogs(List<Dog> dogs) {
-
-        for (Dog dog : dogs) {
-
-            addNewDog(dog.name, dog.dogStatus, dog.visitTime);
+    public void addNewDogs(List<Dog> dogs)
+    {
+        for (Dog dog : dogs)
+        {
+            addNewDog(dog);
         }
     }
 }
---
+++ dog-shelter-example-main/src/main/java/com/example/shelter/db/DogInsertDataAccessByDmitrii.java
@@ -0,0 +1,26 @@
+package com.example.shelter.db;
+
+import java.util.List;
+
+import com.example.shelter.animal.Dog;
+
+public class DogInsertDataAccessByDmitrii implements DogInsertDataAccessInterface
+{
+    @Override
+    public void addNewDogByName(final String name)
+    {
+        String a = "l";
+    }
+
+    @Override
+    public void addNewDog(final Dog dog)
+    {
+
+    }
+
+    @Override
+    public void addNewDogs(final List<Dog> dogs)
+    {
+
+    }
+}
