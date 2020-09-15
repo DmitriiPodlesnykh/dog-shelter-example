23e3d428c9c642337308545d8234b8afe4508d89
Dmitrii Podlesnykh <podlesnykh.dmitrii@gmail.com>
2020-09-15 20:03:29 +0300

    Java Getter/Setter

---
+++ dog-shelter-example-main/src/main/java/com/example/shelter/animal/Fish.java
@@ -0,0 +1,16 @@
+package com.example.shelter.animal;
+
+public class Fish
+{
+    private String name = "Fish";
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(final String name)
+    {
+        this.name = name;
+    }
+}
--- dog-shelter-example-main/src/main/java/com/example/shelter/handler/FishHandler.java
+++ dog-shelter-example-main/src/main/java/com/example/shelter/handler/FishHandler.java
@@ -3,45 +3,15 @@ package com.example.shelter.handler;
 import io.javalin.http.Context;
 import io.javalin.http.Handler;
 
-import com.example.shelter.animal.Fish2;
-import com.example.shelter.animal.Resident;
+import com.example.shelter.animal.Fish;
 
 public class FishHandler implements Handler
 {
     @Override
     public void handle(final Context ctx) throws Exception
     {
-        Resident resident = new Resident()
-        {
-            @Override
-            public void eat()
-            {
-
-            }
-        };
-
-
-        ctx.html("<h1>Fishfish</h1>");
-
-
-
-        Fish2 fish2 = new Fish2()
-        {
-            @Override
-            public void doNothing()
-            {
-                //do it!;
-            }
-        };
-
-        Fish2.FishNonStaticInner fishNonStaticInner= fish2.new FishNonStaticInner();
-
-
-    }
-
-
-    public void check(Fish2 fish2)
-    {
-
+        Fish fish = new Fish();
+        fish.setName("name");
+        ctx.html(fish.getName());
     }
 }
